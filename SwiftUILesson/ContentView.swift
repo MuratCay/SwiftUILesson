114b0bdf5d8ac3043192842f48acd9f3eba87160
muratcay <muratthebestof@gmail.com>
2022-09-11 14:17:36 +0300

    Initial Commit

---
+++ SwiftUILesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILesson
+//
+//  Created by Murat Çay on 11.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILesson/SwiftUILessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILessonApp.swift
+//  SwiftUILesson
+//
+//  Created by Murat Çay on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
